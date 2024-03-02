047c034ba009c5792af2004e8b940d9afba68099
Siarhei Dziki <s.dikiy@strikerz.inc>
2024-03-02 14:56:08 +0100

    removed relay logs

--- src/relay/relay.cxx
+++ src/relay/relay.cxx
@@ -53,10 +53,7 @@ bool relay::run()
 			if (findRes != m_addressMappedChannels.end() && findRes->second.m_peerA && findRes->second.m_peerB)
 			{
 				const auto sendAddr = *findRes->second.m_peerA != *recvAddr ? findRes->second.m_peerA : findRes->second.m_peerB;
-
-				LOG(Verbose, "Relaying packet of {0} bytes from {1} to {2}", bytesRead, sendAddr->toString(), recvAddr->toString());
 				auto bytesSent = m_socket->sendTo(buffer.data(), bytesRead, sendAddr.get());
-
 				continue;
 			}
 			else if (bytesRead == 1024) // const handshake packet size
