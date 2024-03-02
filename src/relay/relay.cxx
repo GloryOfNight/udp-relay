0cedacbda4c6894f9cc1aae2146c6678b51dafc7
Siarhei Dziki <s.dikiy@strikerz.inc>
2024-03-02 13:32:47 +0100

    added debug stuff

--- src/relay/relay.cxx
+++ src/relay/relay.cxx
@@ -30,9 +30,6 @@ channel& relay::createChannel(const guid& inGuid)
 {
 	channel newChannel{};
 	newChannel.m_guid = inGuid;
-
-	LOG(Display, "Created channel with guid: {0}", inGuid.toString());
-
 	return *m_channels.emplace_after(m_channels.before_begin(), newChannel);
 }
 
@@ -63,39 +60,41 @@ bool relay::run()
 
 				continue;
 			}
-
-			if (bytesRead == 1024) // const handshake packet size
+			else if (bytesRead == 1024) // const handshake packet size
 			{
 				const handshake_header* header = reinterpret_cast<const handshake_header*>(buffer.data());
-
-				LOG(Verbose, "Received header: type: {0}, length: {1} from {2}", header->m_type, header->m_length, recvAddr->toString());
 
 				auto nthType = NETWORK_TO_HOST_16(header->m_type);
 				auto nthLength = NETWORK_TO_HOST_16(header->m_length);
 
-				LOG(Verbose, "Swapped header: type: {0}, length: {1} from {2}", nthType, nthLength, recvAddr->toString());
-
 				if (nthType == 0x01 && nthLength == 992) // const handshake packet values
 				{
 					const auto& recvGuid = header->m_id;
-					const guid htnGuid = guid(NETWORK_TO_HOST_16(recvGuid.m_a), NETWORK_TO_HOST_16(recvGuid.m_b), NETWORK_TO_HOST_16(recvGuid.m_c), NETWORK_TO_HOST_16(recvGuid.m_d));
+					const guid htnGuid = guid(NETWORK_TO_HOST_32(recvGuid.m_a), NETWORK_TO_HOST_32(recvGuid.m_b), NETWORK_TO_HOST_32(recvGuid.m_c), NETWORK_TO_HOST_32(recvGuid.m_d));
 
 					auto guidChannel = m_guidMappedChannels.find(htnGuid);
 					if (guidChannel == m_guidMappedChannels.end())
 					{
 						channel& newChannel = createChannel(htnGuid);
 						newChannel.m_peerA = recvAddr;
+						newChannel.m_peerB = nullptr;
 
 						m_guidMappedChannels.emplace(newChannel.m_guid, newChannel);
+
+						LOG(Display, "Created channel with guid: {0}", newChannel.m_guid.toString());
 					}
 					else if (*guidChannel->second.m_peerA != *recvAddr && guidChannel->second.m_peerB == nullptr)
 					{
 						guidChannel->second.m_peerB = recvAddr;
 
-						LOG(Display, "Creating relay for session: {0} with peers: {1}, {2}.", htnGuid.toString(), guidChannel->second.m_peerA->toString(), guidChannel->second.m_peerB->toString());
-
 						m_addressMappedChannels.emplace(guidChannel->second.m_peerA, guidChannel->second);
 						m_addressMappedChannels.emplace(guidChannel->second.m_peerB, guidChannel->second);
+
+						LOG(Display, "Created relay for session: {0} with peers: {1}, {2}.", htnGuid.toString(), guidChannel->second.m_peerA->toString(), guidChannel->second.m_peerB->toString());
+					}
+					else
+					{
+						LOG(Display, "{0} == {1}", guidChannel->second.m_peerA->toString(), recvAddr->toString())
 					}
 				}
 			}
