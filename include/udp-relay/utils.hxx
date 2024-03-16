cd873eb4c17225ae1824bac26891198d111d00ee
Siarhei Dziki <siarhei.dziki@gmail.com>
2024-03-17 00:55:33 +0100

    another linux fix

--- include/udp-relay/utils.hxx
+++ include/udp-relay/utils.hxx
@@ -11,9 +11,9 @@
 #define BYTESWAP32(x) _byteswap_ulong(static_cast<uint32_t>(x))
 #define BYTESWAP64(x) _byteswap_uint64(static_cast<uint64_t>(x))
 #elif defined(__clang__)
-#define BYTESWAP16(x) __bswap16(static_cast<uint16_t>(x))
-#define BYTESWAP32(x) __bswap32(static_cast<uint32_t>(x))
-#define BYTESWAP64(x) __bswap64(static_cast<uint32_t>(x))
+#define BYTESWAP16(x) __bswap_16(static_cast<uint16_t>(x))
+#define BYTESWAP32(x) __bswap_32(static_cast<uint32_t>(x))
+#define BYTESWAP64(x) __bswap_64(static_cast<uint32_t>(x))
 #endif
 
 namespace udprelay::utils
