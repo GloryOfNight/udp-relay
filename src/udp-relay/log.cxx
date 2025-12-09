3158667f6aa9ceb103c1bd2d9c50a2ff0a25cea1
Sergey Dikiy <siarhei.dziki@gmail.com>
2025-12-09 19:59:27 +0100

    modernize log (#19)

---
+++ src/udp-relay/log.cxx
@@ -0,0 +1,6 @@
+// Copyright(c) 2025 Siarhei Dziki aka "GloryOfNight"
+
+#include "udp-relay/log.hxx"
+
+// log.hxx: extern log_level g_runtimeLogLevel;
+log_level g_runtimeLogLevel{log_level::Info};
